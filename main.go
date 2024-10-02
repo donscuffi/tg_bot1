90bb0b2c32fd524b0dacea5d0f7178557205aaa0
donscuffi <kiromagomedov@yandex.ru>
2024-10-03 02:18:43 +0300

    Add files via upload
    commit1

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+const (
+	tgBotHost = "https://api.telegram.org"
+)
+
+func main() {
+	tgClient = telegram.New(tgBotHost, mustToken())
+	//
+	//fetcher = fetcher.New(tgClient)
+	//
+	//processor = processor.New(tgClient)
+	//
+	//consumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	// bot -tg-bot-token 'my token'
+	token := flag.String( // *
+		"token-bot-token",
+		"",
+		"token to access to tg bot",
+	)
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is required")
+	}
+
+	return *token
+}
