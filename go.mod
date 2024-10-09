d787b571ffdba13f7e074fd3063345fc069dd008
donscuffi <kiromagomedov@yandex.ru>
2024-10-09 13:55:24 +0300

    sql

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tg_bot1
 
 go 1.23rc2
+
+require github.com/mattn/go-sqlite3 v1.14.24 // indirect
--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	tgClient "tg_bot1/clients/telegram"
 	event_consumer "tg_bot1/consumer/event-consumer"
 	"tg_bot1/events/telegram"
-	"tg_bot1/storage/files"
+	"tg_bot1/storage/sqlite"
 )
 
 const (
-	tgBotHost   = "api.telegram.org"
-	storagePath = "files_storage"
-	batchSize   = 100
+	tgBotHost         = "api.telegram.org"
+	sqliteStoragePath = "data/sqlite/storage.db"
+	batchSize         = 100
 )
 
 func main() {
+	//s:=files.New(sqliteStoragePath)
+	s, err := sqlite.New(sqliteStoragePath)
+	if err != nil {
+		log.Fatal("can't connect to storage: ", err)
+	}
+
+	if err := s.Init(context.TODO()); err != nil {
+		log.Fatal("can't init storage: ", err)
+	}
+
 	eventsProcessor := telegram.New(
 		tgClient.New(tgBotHost, mustToken()),
-		files.New(storagePath),
+		s,
 	)
 
 	log.Println("service started")
